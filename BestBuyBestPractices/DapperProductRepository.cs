4c17e08776b37d8ade23a5b2ec8b647d52f8f973
Hy Huynh <hyxhuynh@gmail.com>
2020-12-14 02:02:36 -0800

    Added methods for creating a new product, querying the list of products, updating, and deleting a product from the database

---
+++ BestBuyBestPractices/DapperProductRepository.cs
@@ -0,0 +1,58 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace BestBuyBestPractices
+{
+    public class DapperProductRepository : IProductRepository
+    {
+        #region Encapsulation
+        private readonly IDbConnection _connection;
+        // Constructor: create a new instance of the class 
+        public DapperProductRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+        #endregion
+
+        // Create Method
+        public void CreateProduct(string newName, double newPrice, int newCategoryID)
+        {
+            _connection.Execute("INSERT INTO products (Name, Price, CategoryID) VALUES ( @name, @price, @categoryID );", 
+                new { name = newName, price = newPrice, categoryID = newCategoryID });
+        }
+
+        // Read Method
+        public IEnumerable<Product> GetAllProducts()
+        {
+            // Dapper starts here
+            // Dapper extends ===> IDbConnection
+            // Method: return IEnumerable of products
+            return _connection.Query<Product>("SELECT * FROM products;");
+        }
+
+        // Update Method
+        public void UpdateProductName(int productID, string updatedName)
+        {
+            _connection.Execute("UPDATE products SET Name = @updatedName WHERE ProductID = @productID;",
+                new { updatedName = updatedName, productID = productID });
+        }
+
+        // Delete Method
+        public void DeleteProduct(int productID)
+        {
+            // relational database, must delete from all the tables or there will be error
+            _connection.Execute("DELETE FROM reviews WHERE ProductID = @productID;",
+                new { productID = productID});
+            _connection.Execute("DELETE FROM sales WHERE ProductID = @productID;",
+                new { productID = productID });
+            _connection.Execute("DELETE FROM products WHERE ProductID = @productID;",
+                new { productID = productID });
+
+        }
+
+
+    }
+}
--- BestBuyBestPractices/Program.cs
+++ BestBuyBestPractices/Program.cs
@@ -23,14 +23,14 @@ namespace BestBuyBestPractices
 
             IDbConnection conn = new MySqlConnection(connString);
 
-            DapperDepartmentRepository repo = new DapperDepartmentRepository(conn);
+            DapperDepartmentRepository departmentRepo = new DapperDepartmentRepository(conn);
 
             Console.WriteLine("Hello user, here are the current departments: ");
             Console.WriteLine("Please press Enter. . .");
-            Console.WriteLine();
+            Console.ReadLine();
 
-            var depos = repo.GetAllDepartments();
-            Print(depos);
+            var departments = departmentRepo.GetAllDepartments();
+            Print(departments);
 
             Console.WriteLine("Do you want to add a department?");
             string userResponse = Console.ReadLine();
@@ -40,18 +40,43 @@ namespace BestBuyBestPractices
                 Console.WriteLine("What is the name of the new department?");
                 userResponse = Console.ReadLine();
 
-                repo.InsertDepartment(userResponse);
-                Print(repo.GetAllDepartments());
+                departmentRepo.InsertDepartment(userResponse);
+                Print(departmentRepo.GetAllDepartments());
             }
 
+           // PRODUCTS
+            Console.WriteLine("Hello user, here are the current products: ");
+            Console.WriteLine("Please press Enter. . .");
+            Console.ReadLine();
+            DapperProductRepository productRepo = new DapperProductRepository(conn);
+
+            // Create, Update, and Delete Methods
+            // productRepo.CreateProduct("newProduct", 20, 1);
+            // productRepo.UpdateProductName(942, "newStuff");
+            // productRepo.DeleteProduct(942);
+
+            // Read Method
+            // GetAllProducts belongs to the instance of the DapperProductRepository class
+            var products = productRepo.GetAllProducts();         
+            PrintProducts(products);
+
             Console.WriteLine("Have a great day!");
+
         }
 
-        private static void Print(IEnumerable<Department> depos )
+        private static void Print(IEnumerable<Department> departments )
         {
-            foreach (var depo in depos)
+            foreach (var department in departments)
             {
-                Console.WriteLine($"Id: {depo.DepartmentID} Name: {depo.Name}");
+                Console.WriteLine($"Department ID: {department.DepartmentID} Name: {department.Name}");
+            }
+        }
+
+        private static void PrintProducts(IEnumerable<Product> products)
+        {
+            foreach (var product in products)
+            {
+                Console.WriteLine($"Product ID: {product.ProductID} Name: {product.Name}");
             }
         }
 
